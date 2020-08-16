49677b72e967c2ecb238340fa2e235197cbe2a45
Peter Januarius <pete@nexgencodecamp.com.au>
2020-08-16 16:30:26 +1000

    added button to scene

--- scene_intro.js
+++ scene_intro.js
@@ -10,6 +10,8 @@ let Scene_Intro = new Phaser.Class({
 
     preload: function () {
         this.load.image('intro', 'assets/img/intro.png');
+        this.load.image('buttonBG', 'assets/img/button-bg.png');
+        this.load.image('buttonText', 'assets/img/button-text.png');
     },
 
     create: function () {
@@ -17,7 +19,25 @@ let Scene_Intro = new Phaser.Class({
 
         this.add.image(400, 300, 'intro')
 
-        //this.scene.start('sceneB');
+        let bg = this.add.image(0, 0, 'buttonBG').setInteractive();
+        let text = this.add.image(0, 0, 'buttonText');
+        let container = this.add.container(400, 350, [bg, text]);
+
+        bg.on('pointerover', function () {
+
+            this.setTint(0x44ff44);
+
+        });
+
+        bg.on('pointerout', function () {
+
+            this.clearTint();
+
+        });
+
+        bg.on('pointerup', function () {
+            console.log("Button pressed")
+        }, this);
     }
 
 });
