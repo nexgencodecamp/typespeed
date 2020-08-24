ac0d0847e5a4e5fb3806352f49611a12b809f1c3
Peter Januarius <pete@nexgencodecamp.com.au>
2020-08-24 16:28:53 +1000

    Added utilities file

---
+++ phaser_utils.js
@@ -0,0 +1,20 @@
+function centerText(totalWidth, wd) {
+    return (totalWidth / 2) - (wd.width / 2);
+}
+
+function typeWriteText(self, textObj, text, delay, cb) {
+    const length = text.length
+    let i = 0
+    self.time.addEvent({
+        callback: () => {
+            textObj.text += text[i]
+            ++i
+            console.log("textObj length:", textObj.text.length, "text length:", text.length)
+            if (textObj.text.length === text.length) {
+                cb();
+            }
+        },
+        repeat: length - 1,
+        delay: delay || 200
+    })
+}
